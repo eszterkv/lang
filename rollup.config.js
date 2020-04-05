1221fc609ed1e67f372a0f7c1394c97ec2346391
eszter <ekov@protonmail.ch>
2020-04-05 21:11:24 +0100

    create svelte app with mdsvex

---
+++ rollup.config.js
@@ -0,0 +1,120 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import config from 'sapper/config/rollup.js';
+import hljs from 'highlight.js';
+import path from 'path';
+import pkg from './package.json';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import svelte from 'rollup-plugin-svelte';
+import { mdsvex } from 'mdsvex';
+import { terser } from 'rollup-plugin-terser';
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+
+const onwarn = (warning, onwarn) =>
+  (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message))
+    || onwarn(warning);
+
+const preprocess =
+  mdsvex({
+    extension: '.svx',
+    layout: path.join(__dirname, 'src/components/layout.svelte'),
+    markdownOptions: {
+      typographer: true,
+      linkify: true,
+      highlight: (str, lang) => hljs.highlight(str, lang),
+    },
+  });
+
+export default {
+	client: {
+		input: config.client.input(),
+		output: config.client.output(),
+		plugins: [
+			replace({
+				'process.browser': true,
+				'process.env.NODE_ENV': JSON.stringify(mode)
+			}),
+			svelte({
+        preprocess,
+        extensions: ['.svelte', '.svx'],
+				hydratable: true,
+				emitCss: true,
+				dev,
+      }),
+			resolve({
+				browser: true,
+				dedupe: ['svelte']
+			}),
+			commonjs(),
+
+			legacy && babel({
+				extensions: ['.js', '.mjs', '.html', '.svelte'],
+				runtimeHelpers: true,
+				exclude: ['node_modules/@babel/**'],
+				presets: [
+					['@babel/preset-env', {
+						targets: '> 0.25%, not dead'
+					}]
+				],
+				plugins: [
+					'@babel/plugin-syntax-dynamic-import',
+					['@babel/plugin-transform-runtime', {
+						useESModules: true
+					}]
+				]
+			}),
+
+			!dev && terser({
+				module: true
+			})
+		],
+
+		onwarn,
+	},
+
+	server: {
+		input: config.server.input(),
+		output: config.server.output(),
+		plugins: [
+			replace({
+				'process.browser': false,
+				'process.env.NODE_ENV': JSON.stringify(mode)
+			}),
+			svelte({
+        preprocess,
+				generate: 'ssr',
+        extensions: ['.svelte', '.svx'],
+				dev
+			}),
+			resolve({
+				dedupe: ['svelte']
+			}),
+			commonjs()
+		],
+		external: Object.keys(pkg.dependencies).concat(
+			require('module').builtinModules || Object.keys(process.binding('natives'))
+		),
+
+		onwarn,
+	},
+
+	serviceworker: {
+		input: config.serviceworker.input(),
+		output: config.serviceworker.output(),
+		plugins: [
+			resolve(),
+			replace({
+				'process.browser': true,
+				'process.env.NODE_ENV': JSON.stringify(mode)
+			}),
+			commonjs(),
+			!dev && terser()
+		],
+
+		onwarn,
+	},
+};
---
+++ src/components/layout.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<div>
+  <slot />
+</div>
+
+<script>
+  export let title
+</script>
