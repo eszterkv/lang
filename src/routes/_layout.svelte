661f434a67e44312fcc9ae3ff2f7c6c8d40f0f34
eszter <ekov@protonmail.ch>
2020-04-08 23:42:41 +0100

    use consistent naming

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Nav from '../components/Nav.svelte';
+  import Nav from '../components/nav.svelte';
 
   export let segment;
 </script>
@@ -14,7 +14,7 @@
   }
 </style>
 
-<Nav {segment}/>
+<Nav {segment} />
 
 <main>
   <slot></slot>
