f335e42913f2a929c49756ebfc7596fdbe1b8e62
eszter <ekov@protonmail.ch>
2020-04-08 17:38:59 +0100

    retab 🤓

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -13,11 +13,11 @@
 </script>
 
 <script context="module">
-	export async function preload({ params, query }) {
-		const res = await this.fetch(`${params.slug}.json`)
-		const data = await res.json()
+  export async function preload({ params, query }) {
+    const res = await this.fetch(`${params.slug}.json`)
+    const data = await res.json()
 
     if (res.status === 200) return { chapter: data }
     else this.error(res.status, data.message)
-	}
+  }
 </script>
